8b23ceaa363ccc88ae0e30671e97bff11a48f682
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-30 13:14:46 +0900

    docs. 버블 차트 페이지 삽입, 불필요한 내용 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import BubbleChart from '../components/BubbleChart'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = (props: any) => {
@@ -14,20 +15,12 @@ const Home: NextPage = (props: any) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <main className={styles.main}>
+        <section className={styles.bubblechart__wrapper}>
+          <h1>Bubble Chart</h1>
+          <BubbleChart />
+        </section>
+      </main>
     </div>
   )
 }
